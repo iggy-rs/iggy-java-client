d66ee57f3773d799b76dcb3330a7e066d0ebc4ac
Maciej Modzelewski <maciej@modzelewski.dev>
2024-10-20 21:44:52 +0200

    Add producer example

---
+++ examples/simple-producer/src/main/java/rs/iggy/SimpleProducer.java
@@ -0,0 +1,72 @@
+package rs.iggy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rs.iggy.clients.blocking.tcp.IggyTcpClient;
+import rs.iggy.identifier.StreamId;
+import rs.iggy.identifier.TopicId;
+import rs.iggy.message.BigIntegerMessageId;
+import rs.iggy.message.MessageToSend;
+import rs.iggy.message.Partitioning;
+import rs.iggy.message.PartitioningKind;
+import rs.iggy.stream.StreamDetails;
+import rs.iggy.topic.CompressionAlgorithm;
+import rs.iggy.topic.TopicDetails;
+import java.math.BigInteger;
+import java.util.Optional;
+import static java.util.Collections.singletonList;
+import static java.util.Optional.empty;
+
+public class SimpleProducer {
+    private static final String STREAM_NAME = "dev01";
+    private static final StreamId STREAM_ID = StreamId.of(STREAM_NAME);
+    private static final String TOPIC_NAME = "events";
+    private static final TopicId TOPIC_ID = TopicId.of(TOPIC_NAME);
+    private static final Logger log = LoggerFactory.getLogger(SimpleProducer.class);
+
+    public static void main(String[] args) {
+        IggyTcpClient client = new IggyTcpClient("localhost", 8090);
+        client.users().login("iggy", "iggy");
+
+        createStream(client);
+        createTopic(client);
+
+        int counter = 0;
+        while (counter++ < 1000) {
+            var text = "message from simple producer " + counter;
+            var message = new MessageToSend(new BigIntegerMessageId(BigInteger.ZERO), text.getBytes(), empty());
+            client.messages()
+                    .sendMessages(STREAM_ID,
+                            TOPIC_ID,
+                            new Partitioning(PartitioningKind.Balanced, new byte[0]),
+                            singletonList(message));
+            log.debug("Message {} sent", counter);
+        }
+
+    }
+
+    private static void createStream(IggyTcpClient client) {
+        Optional<StreamDetails> stream = client.streams().getStream(STREAM_ID);
+        if (stream.isPresent()) {
+            return;
+        }
+        client.streams().createStream(empty(), STREAM_NAME);
+    }
+
+    private static void createTopic(IggyTcpClient client) {
+        Optional<TopicDetails> topic = client.topics().getTopic(STREAM_ID, TOPIC_ID);
+        if (topic.isPresent()) {
+            return;
+        }
+        client.topics()
+                .createTopic(STREAM_ID,
+                        empty(),
+                        1L,
+                        CompressionAlgorithm.none,
+                        BigInteger.ZERO,
+                        BigInteger.ZERO,
+                        empty(),
+                        TOPIC_NAME);
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,5 @@ include("sdk")
 
 include("simple-consumer-example")
 project(":simple-consumer-example").projectDir = file("examples/simple-consumer")
+include("simple-producer-example")
+project(":simple-producer-example").projectDir = file("examples/simple-producer")
