92d40dee2eab7a063c1e4db30928673e0298e648
Maciej Modzelewski <maciej@modzelewski.dev>
2025-02-17 21:45:32 +0100

    Fix consumer offset tests, add setting consumer offset (#3)

--- java-sdk/src/test/java/rs/iggy/clients/blocking/ConsumerOffsetsClientBaseTest.java
+++ java-sdk/src/test/java/rs/iggy/clients/blocking/ConsumerOffsetsClientBaseTest.java
@@ -6,6 +6,7 @@ import rs.iggy.consumergroup.Consumer;
 import rs.iggy.identifier.ConsumerId;
 import rs.iggy.identifier.StreamId;
 import rs.iggy.identifier.TopicId;
+import java.math.BigInteger;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,11 +25,16 @@ public abstract class ConsumerOffsetsClientBaseTest extends IntegrationTest {
     @Test
     void shouldGetConsumerOffset() {
         // when
+        var consumer = new Consumer(Consumer.Kind.Consumer, ConsumerId.of(1223L));
+        consumerOffsetsClient.storeConsumerOffset(StreamId.of(42L),
+                TopicId.of(42L),
+                Optional.empty(),
+                consumer,
+                BigInteger.ZERO);
         var consumerOffset = consumerOffsetsClient.getConsumerOffset(StreamId.of(42L),
                 TopicId.of(42L),
                 Optional.of(1L),
-                new Consumer(
-                        Consumer.Kind.Consumer, ConsumerId.of(1223L)));
+                consumer);
 
         var nonExistingConsumerOffset = consumerOffsetsClient.getConsumerOffset(StreamId.of(42L),
                 TopicId.of(42L),
